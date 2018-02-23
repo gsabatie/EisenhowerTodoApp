e578ac8640d055f954375258463b17dbc20756cd
sabati_g <sabati_g@epitech.eu>
2018-02-23 16:22:47 +0100

    added cocoapods installation script file

---
+++ README.md
@@ -0,0 +1,8 @@
+# Requirement
+
+- **Cocoapods**
+
+You can use installCocoapods.sh to install cocoapods
+`$ ./installCocoapods.sh`
+
+- **Xcode 9**
---
+++ installPods.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# This will install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# this will install cocoapods
+gem install cocoapods
